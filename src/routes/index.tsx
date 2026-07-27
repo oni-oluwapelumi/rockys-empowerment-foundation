f6e6e31a8011599d21e594cc8d12ac020adaf855
ONI OLUWAPELUMI <Onipelumi41@gmail.com>
2026-07-27 10:19:12 -0300

    Remove registration banner from homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,6 @@ import { Pillars } from "@/components/landing/Pillars";
 import { FounderVision } from "@/components/landing/FounderVision";
 import { Engage } from "@/components/landing/Engage";
 import { Transparency } from "@/components/landing/Transparency";
-import { TrustCredentials } from "@/components/landing/TrustCredentials";
 import { SiteFooter } from "@/components/landing/SiteFooter";
 import { useReveal } from "@/hooks/use-reveal";
 
@@ -20,7 +19,6 @@ function Index() {
       <SiteHeader />
       <main>
         <Hero />
-        <TrustCredentials />
         <Pillars />
         <FounderVision />
         <Transparency />
